e11f61fb660778a0ddd1a3f0d59b67f00b2820b8
ninocaca36 <priyatno.y@gmail.com>
2026-04-19 22:10:39 +0700

    feat: add route definitions for tentang-kami frontend pages

--- src/routes/frontend/about/tentang.php
+++ src/routes/frontend/about/tentang.php
@@ -3,12 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Frontend\TentangController;
 
-Route::prefix('tentang')->group(function () {
+Route::prefix('tentang-kami')->group(function () {
     
     // halaman utama tentang
-    Route::get('/', [TentangController::class, 'index'])->name('tentang.index');
+    Route::get('/', [TentangController::class, 'index'])->name('frontend.tentang-kami.index');
 
     // contoh show detail (optional)
-    Route::get('/{slug}', [TentangController::class, 'show'])->name('tentang.show');
+    Route::get('/{slug}', [TentangController::class, 'show'])->name('frontend.tentang-kami.show');
 
 });
